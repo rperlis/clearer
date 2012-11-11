d334e5458999cd5d9e092e9ab173a2370654e663
rperlis <rperlis@gmail.com>
2012-11-10 20:12:48 -0800

    fixed?

--- server.py
+++ server.py
@@ -83,7 +83,7 @@ class index:
             
             result=calculate_burden.check_medlist(variables)
 
-            print("subsp450_returned",result['subs_p450'])
+            print("listed_CID",result['listed_CID'])
             if result['matched_drugs']==[] : return render.error(variables['Index_drug'],variables['Comparator'])
             else: return render.results(result['matched_drugs'],result['listed_CID'],result['list_by_drug'],result['list_by_ae'],result['annotation_by_drug'],result['ae_score'],result['drug_score'],result['ae_total'],
                                         result['modifiers_p450'],result['subs_p450'])
